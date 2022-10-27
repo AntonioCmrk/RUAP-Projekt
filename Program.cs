fae7cda6083dbef2ad0841c875ef7c882368e3b6
iva hodic <ivahodic@gmal.com>
2022-10-27 18:25:45 +0200

    commit dodan popzdrav

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Antonia Cmrka");
+            Console.WriteLine("Pozdrav iz aplikacije od Antonia Cmrka pozdrav");
         }
     }
 }
